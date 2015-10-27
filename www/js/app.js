f759395e046b5411e4057ce25a57ae289375a21d
Alan Thiessen <AlanDThiessen@gmail.com>
2015-10-27 17:00:26 -0400

    Conversion to Angular Style Guide #02.

--- www/js/app.js
+++ www/js/app.js
@@ -1,46 +1,56 @@
 // Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-theApp = angular.module('starter', ['ionic', 'starter.controllers', 'BibleDB', 'BibleControllers']);
+(function() {
+    'use strict';
 
-theApp.run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
-        }
+    var dependencies = [
+        'ionic',
+        'starter.controllers',
+        'BibleDB',
+        'BibleControllers'
+    ];
 
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
-        }
-    });
-});
+    angular.module('starter', dependencies)
+        .run(Start)
+        .config(Configure);
 
-
-theApp.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/AppRoot.html',
-        controller: 'AppCtrl'
-    });
-
-    $stateProvider.state('app.MyBible', {
-        url: '/myBible',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/BibleView.html',
-                controller: 'PassageCtrl'
+    function Start($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
             }
-        }
-    });
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/myBible');
-});
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    }
+
+    function Configure($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('app', {
+            url: '/app',
+            abstract: true,
+            templateUrl: 'templates/AppRoot.html',
+            controller: 'AppCtrl'
+        });
+
+        $stateProvider.state('app.MyBible', {
+            url: '/myBible',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/BibleView.html',
+                    controller: 'PassageCtrl'
+                }
+            }
+        });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/myBible');
+    }
+
+})();
+
